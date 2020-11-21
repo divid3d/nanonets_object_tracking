c72bc10a1d14df8cf43cfaa676069950cdfa5fa3
Divided <woojciechczop@gmail.com>
2020-11-21 21:11:18 +0100

    added checkpoint training resume support

--- siamese_train.py
+++ siamese_train.py
@@ -28,20 +28,20 @@ Get training data
 """
 
 
-class Config():
-    training_dir = "siamese_train_occlusion/"
-    testing_dir = "siamese_test_occlusion/"
+class Config:
+    training_dir = "my_track_dataset_train/"
+    testing_dir = "my_track_dataset_test/"
     train_batch_size = 128
-    train_number_epochs = 700
+    train_number_epochs = 30
+    model_save_path = ""
+    checkpoint_save_path = ""
+    checkpoint_load_path = ""
 
 
 folder_dataset = dset.ImageFolder(root=Config.training_dir)
 
 transforms = torchvision.transforms.Compose([
     torchvision.transforms.Resize((128, 128)),
-    torchvision.transforms.ColorJitter(hue=.05, saturation=.05),
-    torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms.RandomRotation(20, resample=PIL.Image.BILINEAR),
     torchvision.transforms.ToTensor()
 ])
 
@@ -63,7 +63,6 @@ def get_gaussian_mask():
 
     return mask
 
-
 siamese_dataset = SiameseTriplet(imageFolderDataset=folder_dataset, transform=transforms, should_invert=False)
 net = SiameseNetwork().cuda()
 
@@ -76,18 +75,32 @@ counter = []
 loss_history = []
 iteration_number = 0
 
-train_dataloader = DataLoader(siamese_dataset, shuffle=True, num_workers=14, batch_size=Config.train_batch_size)
+train_dataloader = DataLoader(siamese_dataset, shuffle=False, num_workers=14, batch_size=Config.train_batch_size)
+
+start_iter = 0
+start_epoch = 0
+
+if Config.checkpoint_load_path:
+    checkpoint = torch.load(Config.checkpoint_load_path)
+    net.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    start_iter = checkpoint['iter']
+    start_epoch = checkpoint['epoch']
+    #loss = checkpoint['loss']
+    print("Successfully loaded checkpoint {}".format(Config.checkpoint_load_path))
+
 
 # Multiply each image with mask to give attention to center of the image.
 gaussian_mask = get_gaussian_mask().cuda()
 
 training_start_time = datetime.now()
 
-for epoch in range(0, Config.train_number_epochs):
+
+for epoch in range(start_epoch, Config.train_number_epochs):
 
     epoch_start_time = datetime.now()
 
-    for i, data in enumerate(train_dataloader, 0):
+    for i, data in enumerate(train_dataloader, start_iter):
 
         iteration_start_time = datetime.now()
 
@@ -114,10 +127,23 @@ for epoch in range(0, Config.train_number_epochs):
             plt.plot(counter, loss_history)
             plt.savefig('train_loss.png')
             print("Saved train_loss.png")
+            if Config.checkpoint_save_path:
+                checkpoint_name = "checkpoint_i{}_e{}.pt"
+                checkpoint_save_path = os.path.join(Config.checkpoint_save_path, checkpoint_name)
+                torch.save({
+                    'iter': i,
+                    'epoch': epoch,
+                    'model_state_dict': net.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': triplet_loss.item(),
+                }, checkpoint_save_path)
+                print("Checkpoint {} saved in {} successfully".format(checkpoint_name, checkpoint_save_path))
 
-    if not os.path.exists('saved_models/'):
-        os.mkdir('saved_models')
-    torch.save(net, 'saved_models/model' + str(epoch) + '.pt')
+    start_iter = 0
+
+    if not os.path.exists(Config.model_save_path):
+        os.mkdir(Config.model_save_path)
+    torch.save(net, Config.model_save_path + "model{}.pt".format(epoch + 1))
 
     epoch_elapsed_time = datetime.now() - epoch_start_time
     print("Training epoch time: {}".format(epoch_elapsed_time))
